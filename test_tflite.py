c1bfbe3b3f1e596f88a0397e783cfe76222e9844
cardinaldeacre <cardinaldeacre@gmail.com>
2026-01-07 10:43:24 +0700

    change model

--- test_tflite.py
+++ test_tflite.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("model/best_float32.tflite")
-model.predict(source=0, imgsz=640, conf=0.25, show=True) # webcam
+model = YOLO("model/yolo8x320.tflite")
+model.predict(source=0, imgsz=320, conf=0.25, show=True) # webcam
 
 print("done")
